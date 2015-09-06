7267881a40f02e3cbab599f0b160dc94caf2299b
Suzane Sant Ana <tetestonaldo@gmail.com>
2015-09-06 13:14:08 -0300

    Delete games.js

--- games.js
+++
@@ -1,8 +0,0 @@
-var games = Array("http://www.shooter-bubble.com/bubbleshooter2.swf",
-                  "http://www.shooter-bubble.com/puzzle-bobble.swf",
-                  "http://www.shooter-bubble.com/number-lines.swf",
-                  "http://randolphburt.github.io/Centipede-Angular/");
-
-var getGame = function(){
-  return games[Math.floor(Math.random() * games.length)];
-};
