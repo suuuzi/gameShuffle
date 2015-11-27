dd6642cd8d7188716bd13ebf5124f5a0afa290bb
Suzane Sant Ana <tetestonaldo@gmail.com>
2015-11-26 22:39:57 -0200

    2048

--- games.js
+++ games.js
@@ -2,7 +2,8 @@ var games = Array("http://www.shooter-bubble.com/bubbleshooter2.swf",
                   "http://www.shooter-bubble.com/puzzle-bobble.swf",
                   "http://www.shooter-bubble.com/number-lines.swf",
                   "http://randolphburt.github.io/Centipede-Angular/",
-                  "https://www.solitr.com/");
+                  "https://www.solitr.com/",
+                  "https://gabrielecirulli.github.io/2048/");
 
 var getGame = function(){
   return games[Math.floor(Math.random() * games.length)];
