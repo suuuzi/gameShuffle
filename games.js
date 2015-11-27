8817edfbf1c194c2fc7131da2113fefa1c752200
Suzane Sant Ana <tetestonaldo@gmail.com>
2015-11-26 22:26:16 -0200

    add solitaire

--- games.js
+++ games.js
@@ -1,7 +1,8 @@
 var games = Array("http://www.shooter-bubble.com/bubbleshooter2.swf",
                   "http://www.shooter-bubble.com/puzzle-bobble.swf",
                   "http://www.shooter-bubble.com/number-lines.swf",
-                  "http://randolphburt.github.io/Centipede-Angular/");
+                  "http://randolphburt.github.io/Centipede-Angular/",
+                  "https://www.solitr.com/");
 
 var getGame = function(){
   return games[Math.floor(Math.random() * games.length)];
