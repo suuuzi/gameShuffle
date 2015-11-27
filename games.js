3750fd8e5324df6b59caa68fe8454d4ac46c53d9
Suzane Sant Ana <tetestonaldo@gmail.com>
2015-11-26 22:42:08 -0200

    sudoku

--- games.js
+++ games.js
@@ -3,7 +3,8 @@ var games = Array("http://www.shooter-bubble.com/bubbleshooter2.swf",
                   "http://www.shooter-bubble.com/number-lines.swf",
                   "http://randolphburt.github.io/Centipede-Angular/",
                   "https://www.solitr.com/",
-                  "https://gabrielecirulli.github.io/2048/");
+                  "https://gabrielecirulli.github.io/2048/",
+                  "http://www.sudoku.com/");
 
 var getGame = function(){
   return games[Math.floor(Math.random() * games.length)];
