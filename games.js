bff505a881dec840072533baf709203abf8976f2
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-04-15 20:30:35 -0300

    Update games.js
    new game

--- games.js
+++ games.js
@@ -5,7 +5,8 @@ var games = Array("http://www.shooter-bubble.com/bubbleshooter2.swf",
                   "https://www.solitr.com/",
                   "https://gabrielecirulli.github.io/2048/",
                   "http://www.sudoku.com/",
-                  "http://gariel.pythonanywhere.com/static/Gro.swf");
+                  "http://gariel.pythonanywhere.com/static/Gro.swf",
+                  "http://slither.io/");
 
 var getGame = function(){
   return games[Math.floor(Math.random() * games.http://gariel.pythonanywhere.com/static/Gro.swflength)];
